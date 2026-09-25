a682f6a4892bd28d43f9cc55575b6fd152f38ff6
Zsolt Veres <vereszsolt06@gmail.com>
2026-09-25 21:55:37 +0200

    Initial commit - Application.java

---
+++ src/main/java/hu/unideb/inf/corp/Application.java
@@ -0,0 +1,11 @@
+package hu.unideb.inf.corp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
